449a85bb15d4477a5d1b5de05fcc7c17a28c746a
ivanwinata96 <ivanwinata96@users.noreply.github.com>
2020-11-02 13:32:16 +0700

    Initial Commit

---
+++ recipe app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  recipe app
+//
+//  Created by Ivan Winata on 02/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ recipe appTests/recipe_appTests.swift
@@ -0,0 +1,33 @@
+//
+//  recipe_appTests.swift
+//  recipe appTests
+//
+//  Created by Ivan Winata on 02/11/20.
+//
+
+import XCTest
+@testable import recipe_app
+
+class recipe_appTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
